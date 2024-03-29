eb26be208ff2d1c3402cf58982a788f2275779fa
ricksclone <haveyoubean@gmail.com>
2024-03-29 15:20:44 +0900

    featserviceinfo

--- src/app/main/main.tsx
+++ src/app/main/main.tsx
@@ -1,110 +1,54 @@
-"use client";
 import React, { FunctionComponent } from "react";
-import { Layout, theme, Typography, Space } from "antd";
-import ResponseCarousel from "./responseCarousel";
 
-const { Title, Text, Link } = Typography;
-const { Content, Footer } = Layout;
+const blueButtonStyle = {
+	backgroundColor: 'blue',
+	color: 'white',
+	padding: '20px 30px',
+	borderRadius: '75.01px',
+	border: 'none',
+	cursor: 'pointer',
+	display: 'inline-flex',  // 버튼을 인라인 블록 요소로 지정하여 문장 내에 삽입
+	marginTop: '85px', // 버튼과 문장 사이에 간격 추가
+	alignItems: 'center', // 아이콘과 문장 수직 가운데 정렬
+	textDecoration: 'none', //링크 밑줄효과 제거
+  };
+  
+  const buttonTextStyle = {
+	marginRight: '10px', // 텍스트와 이미지 사이 간격 추가
+	fontSize: '24px', // 버튼 내 텍스트 크기 
+  };
 
-interface IMainProps {}
+  
 
-const Main: FunctionComponent<IMainProps> = (props) => {
-	const {
-		token: { colorBgContainer, borderRadiusLG },
-	} = theme.useToken();
+function App() {
+    return (
+		<body style={{
+            backgroundColor: 'black', // 배경색을 검정색으로 설정
+            color: 'white', // 글자색을 하얀색으로 설정
+            margin: '0' , // body의 기본 마진을 제거
+			overflowY: 'auto', // 수직 스크롤 가능하도록 설정
+			backgroundImage: 'url("/images/background.png")', //백그라운드 이미지 설정
+			backgroundSize: 'cover', // 배경 이미지를 화면에 꽉 채우도록 설정
+        }}>
+            <div style={{ textAlign: 'center', marginTop: '300px' }}>
+			<div style={{
+                    fontFamily: 'Pretendard-SemiBold', // Pretendard-Bold 폰트 사용
+                    fontSize: '60px',
+                    lineHeight: '150%',
+                }}>
+                    사이드프로젝트를<br/>시작하는 가장 쉽고 빠른 방법
+                </div>
+                <div style={{ marginTop: '30px', fontSize: '20px', fontFamily: 'Pretendard-Light'}}>
+                    개발자, 디자이너, 기획자 등 IT 업계 검증 가능한 다양한 팀원들을 만나보세요.
+                </div>
+				<a href="https://docs.google.com/forms/d/e/1FAIpQLSeWhJrzkMPPpc-gRmurB45bHE3Xu8ga3-dbpfQej1NHXte9yw/viewform" style={blueButtonStyle}>
+                    <span style={buttonTextStyle}>베타서비스 이용 신청하기</span>
+                    <img  src="/images/ei_arrow-left.png" // public > images아이콘 다운받아서 쓸 때 화질 계속 깨지는 이슈 확인
+					 alt="button-icon" style={{ width: '35px', height: '35px' }} />  
+				</a>
+            </div>
+        </body>
+    );
+}
 
-	return (
-		<Layout>
-			<Content style={{ margin: "24px 16px 0" }}>
-				<div
-					style={{
-						padding: 24,
-						textAlign: "center",
-						background: colorBgContainer,
-						borderRadius: borderRadiusLG,
-					}}
-				>
-					<ResponseCarousel
-						items={[
-							{
-								title: "Swiper Carousel Example",
-								text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos mollitia sed quod consectetur at quam dolore praesentium neque eos assumenda iusto nam laborum laboriosam odio blanditiis possimus accusantium recusandae porro exercitationem itaque",
-								imageUrl: "http://placehold.it/350x150",
-							},
-							{
-								title: "Swiper Carousel Example",
-								text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos mollitia sed quod consectetur at quam dolore praesentium neque eos assumenda iusto nam laborum laboriosam odio blanditiis possimus accusantium recusandae porro exercitationem itaque",
-								imageUrl: "http://placehold.it/350x150",
-							},
-							{
-								title: "Swiper Carousel Example",
-								text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quos mollitia sed quod consectetur at quam dolore praesentium neque eos assumenda iusto nam laborum laboriosam odio blanditiis possimus accusantium recusandae porro exercitationem itaque",
-								imageUrl: "http://placehold.it/350x150",
-							},
-						]}
-					/>
-				</div>
-			</Content>
-			<Content style={{ margin: "24px 16px 0" }}>
-				<div
-					style={{
-						padding: 24,
-						minHeight: 360,
-						background: colorBgContainer,
-						borderRadius: borderRadiusLG,
-					}}
-				>
-					테스트 텍스트
-					<Title>h1. Ant Design</Title>
-					<Title level={2}>h2. Ant Design</Title>
-					<Title level={3}>h3. Ant Design</Title>
-					<Title level={4}>h4. Ant Design</Title>
-					<Title level={5}>h5. Ant Design</Title>
-				</div>
-			</Content>
-			<Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
-				<div
-					style={{
-						padding: 24,
-						background: colorBgContainer,
-						borderRadius: borderRadiusLG,
-					}}
-				>
-					<Space direction="vertical">
-						<Text>Ant Design (default)</Text>
-						<Text type="secondary">Ant Design (secondary)</Text>
-						<Text type="success">Ant Design (success)</Text>
-						<Text type="warning">Ant Design (warning)</Text>
-						<Text type="danger">Ant Design (danger)</Text>
-						<Text disabled>Ant Design (disabled)</Text>
-						<Text mark>Ant Design (mark)</Text>
-						<Text code>Ant Design (code)</Text>
-						<Text keyboard>Ant Design (keyboard)</Text>
-						<Text underline>Ant Design (underline)</Text>
-						<Text delete>Ant Design (delete)</Text>
-						<Text strong>Ant Design (strong)</Text>
-						<Text italic>Ant Design (italic)</Text>
-						<Link href="https://ant.design" target="_blank">
-							Ant Design (Link)
-						</Link>
-					</Space>
-					<p>long content</p>
-					{
-						// indicates very long content
-						Array.from({ length: 100 }, (_, index) => (
-							<React.Fragment key={index}>
-								{index % 20 === 0 && index ? "more" : "..."}
-								<br />
-							</React.Fragment>
-						))
-					}
-				</div>
-			</Content>
-			<Footer style={{ textAlign: "center" }}>
-				Sprinter ©{new Date().getFullYear()} Created by dongho1596
-			</Footer>
-		</Layout>
-	);
-};
-
-export default Main;
+export default App;
