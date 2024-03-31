b101477fb4c53eff23e34c45c63639b89a5ec50b
Teddy (Dongho) <gongdongho12@gmail.com>
2024-04-01 00:32:26 +0900

    chore: 머지 오류 수정 및 introBox 컴포넌트 반영

--- src/app/main/main.tsx
+++ src/app/main/main.tsx
@@ -1,12 +1,10 @@
 "use client";
-import React, { FunctionComponent, useState, useEffect } from "react";
-import { Layout, theme, Typography, Space, Flex } from "antd";
-import ResponseCarousel from "./responseCarousel";
+import React, { FunctionComponent } from "react";
+import { Layout, theme, Typography, Flex } from "antd";
 import "../fonts/font.css";
-<<<<<<< HEAD
-=======
 import { white, orange, primary } from "@/app/color";
 import IntroBox from "@/app/component/IntroBox";
+import MainHeader from "@/app/mainheader";
 
 /*
 
@@ -22,8 +20,6 @@ import IntroBox from "@/app/component/IntroBox";
 8.서비스 Q&A 빛 서비스 소개 헤더 주석처리 
 
 */
->>>>>>> 5e349ffe89f2efa219a2655538c3f7a1fc3e1313
-
 const { Title, Text, Link } = Typography;
 const { Content, Footer } = Layout;
 
@@ -35,21 +31,10 @@ const Main: FunctionComponent<IMainProps> = (props) => {
 	} = theme.useToken();
 
 	return (
-<<<<<<< HEAD
-			<Layout style={{ backgroundColor: "black" }}>
-				<Content style={{ margin: "24px 16px 0" }}>
-					<div
-						style={{
-							padding: 24,
-							textAlign: "center",
-						}}
-					>
-=======
 		<>
 			<MainHeader>
 				<Layout style={{ color: white }}>
 					<Content style={{ margin: "24px 16px 0" }}>
->>>>>>> 5e349ffe89f2efa219a2655538c3f7a1fc3e1313
 						<div
 							style={{
 								padding: 24,
@@ -161,11 +146,7 @@ const Main: FunctionComponent<IMainProps> = (props) => {
 
 							{/* 두번째 메인 설명글과 이미지 첨부 구현  */}
 
-<<<<<<< HEAD
-							<div style={{ position: 'relative', top: '40px', zIndex: '0'  }}>
-=======
 							<div style={{ position: "relative" }}>
->>>>>>> 5e349ffe89f2efa219a2655538c3f7a1fc3e1313
 								<img
 									src="../mainimage2.png"
 									alt="mainimage2"
@@ -190,40 +171,6 @@ const Main: FunctionComponent<IMainProps> = (props) => {
 								>
 									IT 업계 협업 풀 스프린터
 								</Typography.Text>
-<<<<<<< HEAD
-								<img src="../ring.png" alt="ring" style={{ // 링 모양 이미지 스타일 조정 
-									position: 'absolute',
-									transform: 'translate(-50%, -49%)',
-									left: '50%',
-									top: 'calc(50% + 60px)',
-									zIndex: '-1',
-									maxWidth: '104%',
-									height: 'auto',
-								}} />
-							</div>
-							<Footer style={{ textAlign: "center", marginTop: "100px",  position: "relative"}}>
-								<div style={{ 	// CTA 버튼 스타일 지정
-									margin: '20px auto',
-									width: 'fit-content',
-								}}>
-									<a href="https://docs.google.com/forms/d/e/1FAIpQLSeWhJrzkMPPpc-gRmurB45bHE3Xu8ga3-dbpfQej1NHXte9yw/viewform" style={{
-										display: 'inline-flex', // 버튼 리다이렉션 주소 지정 및 버튼 내 텍스트 스타일
-										alignItems: 'center',
-										backgroundColor: '#0012FA',
-										color: 'white',
-										padding: '13px 28px',
-										borderRadius: '50px',
-										textDecoration: 'none',
-										fontSize: '21px',
-										fontFamily: 'Pretendard-Medium',
-										zIndex: 1,
-									}}>
-										<span style={{ marginRight: '10px' , }}>베타서비스 이용 신청하기</span>
-										<img src="../ei_arrow-left.png" alt="button-icon" style={{ width: '35px', height: '35px' }} />
-									</a>
-								</div>
-							</Footer>
-=======
 
 								<Typography.Text
 									style={{
@@ -244,79 +191,9 @@ const Main: FunctionComponent<IMainProps> = (props) => {
 									베타서비스를 오픈했어요.
 								</Typography.Text>
 							</div>
->>>>>>> 5e349ffe89f2efa219a2655538c3f7a1fc3e1313
 						</div>
 					</Content>
 
-<<<<<<< HEAD
-						{/* 두번째 메인 설명글과 이미지 첨부 구현  */}
-
-						<div style={{ position: 'relative' }}>
-							<img src="../mainimage2.png" alt="mainimage2" style={{
-								maxWidth: '73%', // 두번째 메인 이미지 스타일 
-								height: 'auto',
-								marginTop: '40px',
-								zIndex: '-2'
-							}} />
-							<Typography.Text style={{ //이미지 아래 메인 설명글 스타일
-								position: 'absolute',
-								fontFamily: 'Pretendard-SemiBold',
-								fontSize: '26px',
-								color: 'white',
-								bottom: '120px',
-								left: '50%',
-								transform: 'translate(-50%, -50%)',
-							}}>
-								IT 업계 협업 풀 스프린터
-							</Typography.Text>
-
-							<Typography.Text style={{ //이미지 아래 서브 설명글 스타일
-								position: 'absolute',
-								fontFamily: 'Pretendard-Regular',
-								fontSize: '16px',
-								color: 'white',
-								top: '750px',
-								left: '50%',
-								transform: 'translate(-50%, -50%)',
-							}}>
-								<br />사이드프로젝트 진행에서 가장 긴 시간이 걸리는 팀 모집 과정을<br />
-								가장 간편하게 진행할 수 있는 IT 업계 협업 풀 스프린터 베타서비스를 오픈했어요.
-							</Typography.Text>
-
-						</div>
-					</div>
-				</Content>
-
-
-				{/* 세번째 메인 설명글과 동영상 첨부 작업 => 동영상 후 순위 */}
-
-				<Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
-					<div
-						style={{
-							padding: 24,
-						}}
-					>
-						{/* 3-1 기능 설명 문구 및 도형 구성  */}
-						<div style={{ position: 'relative', marginTop: '30px', textAlign: 'left', marginLeft: '100px', display: 'flex', flexDirection: 'column' }}>
-							<div style={{ width: '180px', height: '50px', backgroundColor: '#0012FA', borderRadius: '15px', textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-								{/* 도형 안의 프로필 등록하기 텍스트 */}
-								<p style={{ fontFamily: 'Pretendard-Medium', fontSize: '24px', color: 'white', margin: '0' }}>
-									프로필 등록하기
-								</p>
-							</div>
-							{/* 도형 아래 메인 텍스트 */}
-							<p style={{ fontFamily: 'Pretendard-SemiBold', fontSize: '45px', marginTop: '25px' }}>
-								내 프로필 등록<br />
-								<span style={{ color: '#FFAD0E', lineHeight: '1.5' }}>1분</span> 안에 간편하게
-							</p>
-							{/* 도형 아래 서브 텍스트 */}
-							<p style={{ fontFamily: 'Pretendard-Regular', fontSize: '16px', marginTop: '-20px', lineHeight: '1.5' }}>
-								기존에 사용하던 링크드인, 노션, 깃헙과 같은 기타 여러 링크로<br />
-								복잡하게 프로필 및 커리어 내용을 작성할 필요없이,<br />
-								1분 안에 내 프로필 등록이 가능해요.
-							</p>
-						</div>
-=======
 					{/* 세번째 메인 설명글과 동영상 첨부 작업 => 동영상 후 순위 */}
 
 					<Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
@@ -358,102 +235,7 @@ const Main: FunctionComponent<IMainProps> = (props) => {
 								</div>
 							</IntroBox>
 
-							{/* 3-1 기능 설명 문구 및 도형 구성  */}
-							<div>
-								<div
-									style={{
-										width: "180px",
-										height: "50px",
-										backgroundColor: primary,
-										borderRadius: "15px",
-										textAlign: "center",
-										display: "inline-flex",
-										justifyContent: "center",
-										alignItems: "center",
-									}}
-								>
-									{/* 도형 안의 프로필 등록하기 텍스트 */}
-									<div
-										style={{
-											fontFamily: "Pretendard-Medium",
-											fontSize: "24px",
-											color: white,
-											margin: "0",
-										}}
-									>
-										프로필 등록하기
-									</div>
-								</div>
-								{/* 도형 아래 메인 텍스트 */}
-								<div
-									style={{
-										fontFamily: "Pretendard-SemiBold",
-										fontSize: "45px",
-										marginTop: "25px",
-									}}
-								>
-									내 프로필 등록
-									<br />
-									<span style={{ color: orange, lineHeight: "1.5" }}>
-										1분
-									</span>{" "}
-									안에 간편하게
-								</div>
-								{/* 도형 아래 서브 텍스트 */}
-								<div
-									style={{
-										fontFamily: "Pretendard-Regular",
-										fontSize: "16px",
-										marginTop: 30,
-										lineHeight: "1.5",
-									}}
-								>
-									기존에 사용하던 링크드인, 노션, 깃헙과 같은 기타 여러 링크로
-									<br />
-									복잡하게 프로필 및 커리어 내용을 작성할 필요없이,
-									<br />
-									1분 안에 내 프로필 등록이 가능해요.
-								</div>
-							</div>
->>>>>>> 5e349ffe89f2efa219a2655538c3f7a1fc3e1313
-
-							{/* 3-2 기능 설명 문구 및 도형 구성  */}
-							<div
-								style={{
-									width: "100%",
-									position: "relative",
-									marginTop: "100px",
-									textAlign: "left",
-									display: "flex",
-									flexDirection: "column",
-								}}
-							>
-								<div>
-									<div
-										style={{
-											width: "auto",
-											height: "50px",
-											backgroundColor: primary,
-											borderRadius: "15px",
-											textAlign: "center",
-											display: "inline-flex",
-											justifyContent: "center",
-											alignItems: "center",
-										}}
-									>
-										{/* 도형 안의 프로필 등록하기 텍스트 */}
-										<div
-											style={{
-												fontFamily: "Pretendard-Medium",
-												fontSize: "24px",
-												color: white,
-												margin: "0",
-											}}
-										>
-											IT 업계 내 인재풀
-										</div>
-									</div>
-								</div>
+							<IntroBox title={"IT 업계 내 인재풀"} right>
 								<div
 									style={{
 										fontFamily: "Pretendard-SemiBold",
@@ -474,7 +256,6 @@ const Main: FunctionComponent<IMainProps> = (props) => {
 										fontSize: "16px",
 										marginTop: "30px",
 										lineHeight: "1.5",
-										textAlign: "right",
 									}}
 								>
 									개발자, 디자이너, 기획자 등 프로필 등록을 완료한 유저들은
@@ -482,41 +263,9 @@ const Main: FunctionComponent<IMainProps> = (props) => {
 									팀원 구인 게시글 작성 및 개별 프로필 컨택 기능을 사용할 수
 									있어요.
 								</div>
-							</div>
+							</IntroBox>
 
-<<<<<<< HEAD
-						{/* 3-2 기능 설명 문구 및 도형 구성  */}
-						<div style={{ position: 'relative', marginTop: '240px', textAlign: 'left', display: 'flex', flexDirection: 'column' }}>
-							<div style={{ display: 'flex', justifyContent: 'flex-end', marginRight: '100px' }}>
-								<div style={{ width: '200px', height: '50px', backgroundColor: '#0012FA', borderRadius: '15px', textAlign: 'center', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-=======
-							{/* 3-3 협업 과정의 신뢰성 구성  */}
-							<div>
-								<div
-									style={{
-										width: "180px",
-										height: "50px",
-										backgroundColor: primary,
-										borderRadius: "15px",
-										textAlign: "center",
-										display: "flex",
-										justifyContent: "center",
-										alignItems: "center",
-									}}
-								>
->>>>>>> 5e349ffe89f2efa219a2655538c3f7a1fc3e1313
-									{/* 도형 안의 프로필 등록하기 텍스트 */}
-									<div
-										style={{
-											fontFamily: "Pretendard-Medium",
-											fontSize: "24px",
-											color: "white",
-											margin: "0",
-										}}
-									>
-										협업 과정의 신뢰성
-									</div>
-								</div>
+							<IntroBox title={"협업 과정의 신뢰성"}>
 								{/* 도형 아래 메인 텍스트 */}
 								<div
 									style={{
@@ -545,48 +294,7 @@ const Main: FunctionComponent<IMainProps> = (props) => {
 									역량입니다. 함께할 팀원들이 프로젝트를 끝까지 완수할 수 있는
 									사람인지, 서비스 내 협업 점수표를 확인 해보세요.
 								</div>
-							</div>
-<<<<<<< HEAD
-							{/* 도형 아래 메인 텍스트 */}
-							<p style={{ fontFamily: 'Pretendard-SemiBold', fontSize: '45px', marginTop: '25px', textAlign: 'right', marginRight: '100px' }}>
-								함께 <span style={{ color: '#FFAD0E', lineHeight: '1.5' }}>성장</span>할<br />좋은 팀과 팀원 찾기
-							</p>
-							{/* 도형 아래 서브 텍스트 */}
-							<p style={{ fontFamily: 'Pretendard-Regular', fontSize: '16px', marginTop: '-15px', lineHeight: '1.5', textAlign: 'right', marginRight: '100px' }}>
-								개발자, 디자이너, 기획자 등 프로필 등록을 완료한 유저들은<br />
-								팀원 구인 게시글 작성 및 개별 프로필 컨택 기능을 사용할 수 있어요.
-							</p>
-						</div>
-					</div>
-				</Content>
-
-
-
-				{/* 네번째 메인 설명글 작업  */}
-				<Content style={{ padding: 0, overflow: "hidden", marginTop: '240px', width: "100vw" }}>
-					<div
-						style={{
-							backgroundColor: "#0012FA",
-							height: 800,
-							borderRadius: 0,
-							width: "100vw", // 화면 전체 너비로 설정
-							position: "relative", // 부모 요소 설정
-							left: "50%", // 가운데 정렬을 위해 왼쪽으로 50% 이동
-							transform: "translateX(-50%)", // 왼쪽으로 이동한 거리의 반대 방향으로 다시 이동하여 가운데 정렬
-						}}
-					>
-						{/* 도형 내용 */}
-					</div>
-				</Content>
-
-
-				{/* <Footer style={{ textAlign: "center" }}>
-					Sprinter ©{new Date().getFullYear()} Created by dongho1596
-				</Footer>  */}
-
-
-			</Layout>
-=======
+							</IntroBox>
 						</div>
 					</Content>
 					{/* <Footer style={{ textAlign: "center" }}>
@@ -602,7 +310,6 @@ const Main: FunctionComponent<IMainProps> = (props) => {
 				여기에 네번쨰 설명글 넣어주시면 좋을 것 같아요
 			</Flex>
 		</>
->>>>>>> 5e349ffe89f2efa219a2655538c3f7a1fc3e1313
 	);
 };
 
